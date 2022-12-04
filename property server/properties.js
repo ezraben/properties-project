f88741e65a3662b5a0e405ca8c07accff835a00b
ezraben <64844558+ezraben@users.noreply.github.com>
2022-12-04 15:32:54 +0200

    Add files via upload

---
+++ property server/properties.js
@@ -0,0 +1,287 @@
+const { query } = require("express");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+// const multer = require("multer");
+
+// const upLoadMulter = multer({ dest: "uploads/" });
+const upLoadMulter = require("../../config/multer");
+// const upLoadMulter = require("../../config/multer");
+const CustomMsg = require("../../classes/CustomMsg");
+
+const Properties = require("../../models/properties.model");
+const propertiesModel = require("../../models/properties.model");
+
+const propertiesValidation = require("../../validation/property.validation");
+
+// from here multer until where i start CRUD
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads");
+//   },
+//   fileaddress: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldaddress + "-" + uniqueSuffix);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// until here working up load also with react - not mullter
+///////////////////////////////////////////
+// router.post("/", upload.single("propertyImg"), async (req, res) => {
+//   // router.post("/properties", upload.single("propertyImg"), async (req, res) => {
+//   try {
+//     const validateValue = await propertiesValidation.validatePropertySchema(
+//       req.body
+//     );
+//     if (validateValue) {
+//       console.log(validateValue);
+
+//       const newUserData = await propertiesModel.insertProperty(
+//         // const newUserData = await propertiesModel.insertProperty(
+//         // validateValue.img,
+//         validateValue.price,
+//         validateValue.description,
+//         validateValue.address
+//       );
+//       res.json("property created successfully");
+//     }
+//     console.log(req.body);
+//   } catch (err) {
+//     console.log(err);
+//     res.json(err);
+//   }
+// });
+// until here working up load also with react - not mullter
+///////////////////////////////////////////
+
+//ffrom here befor changes on multer on the flight
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads");
+//   },
+//   fileaddress: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldaddress + "-" + uniqueSuffix);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+/////////////////////////////////////////////
+//ipi tyhat works with react but no multer
+// router.post("/", upload.single("propertyImg"), async (req, res) => {
+//   try {
+//     const validateValue = await propertiesValidation.validatePropertySchema(
+//       req.body
+//     );
+
+//     if (validateValue) {
+//       const newUserData = await propertiesModel.insertProperty(
+//         validateValue.img,
+//         validateValue.price,
+//         validateValue.description,
+//         validateValue.address
+//       );
+//       res.json("property created successfully");
+//     }
+//     console.log(req.body);
+//   } catch (err) {
+//     res.json(err);
+//     console.log(err);
+//   }
+// });
+////////////////////////////////////////////////////
+//until here ipi hat works with react but no multer
+
+router.post("/", upLoadMulter.single("propertyImg"), async (req, res) => {
+  try {
+    const validateValue = await propertiesValidation.validatePropertySchema(
+      req.body
+    );
+
+    if (validateValue) {
+      const newUserData = await propertiesModel.insertProperty(
+        // req.file.filename,
+        validateValue.price,
+        validateValue.description,
+        validateValue.address
+        // validateValue.img
+        // req.file.filename
+        // validateValue.propertyImg,
+        // validateValue.price,
+        // validateValue.description,
+        // validateValue.address
+        // req.file.filename
+      );
+      console.log("req.file", req.file);
+      res.json("property created successfully");
+    }
+    console.log(req.body);
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
+});
+
+///////////////////////////////////////
+// until here multer, from here CRUD
+router.get("/", async (req, res) => {
+  try {
+    const properties = await propertiesModel.selectAllProperties();
+    res.json(properties);
+  } catch (err) {
+    res.json(err);
+  }
+});
+router.post("/filter", async (req, res) => {
+  try {
+    // const filterBy = { address: req.body };
+
+    const properties = await propertiesModel.selectPropertyByAddress({
+      // searchInpt: req.body.searchInpt,
+      address: req.body.address,
+    });
+    // const properties = await propertiesModel.selectPropertyByAddress({
+    //   // searchInpt: req.body.searchInpt,
+    //   address: req.body.address,
+    // });
+    console.log(properties);
+    console.log(req.body);
+
+    res.json(properties);
+  } catch (err) {
+    res.json(err);
+  }
+});
+// router.get("/filter", async (req, res) => {
+//   try {
+//     // const filterBy = { address: req.body };
+
+//     const properties = await propertiesModel.selectPropertyByAddress({
+//       address: req.body.address,
+//     });
+
+//     res.json(properties);
+//   } catch (err) {
+//     res.json(err);
+//   }
+// });
+router.post("/filterByPrice", async (req, res) => {
+  try {
+    // const filterBy = { address: req.body };
+
+    const properties = await propertiesModel.selectPropertyByMaxPrice({
+      price: req.body.price,
+    });
+
+    res.json(properties);
+  } catch (err) {
+    res.json(err);
+  }
+});
+router.post("/filterByMinPrice", async (req, res) => {
+  try {
+    // const filterBy = { address: req.body };
+
+    const properties = await propertiesModel.selectPropertyByMinPrice({
+      price: req.body.price,
+    });
+
+    res.json(properties);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+///////////////////////////////////////////
+
+// router.patch("/:id", async (req, res) => {
+//   try {
+//     const id = req.query._id;
+//     const validateValue = await propertiesValidation.validatePropertySchema(
+//       req.body
+//     );
+
+//     if (validateValue) {
+//       const newUserData = await propertiesModel.findByIdAndUpdate(id, {
+//         price: validateValue.price,
+//         description: validateValue.description,
+//         address: validateValue.address,
+//         // validateValue.img,
+//       });
+//       res.json("property upDated  successfully");
+//     }
+//     console.log(req.body);
+//   } catch (err) {
+//     res.json(err);
+//   }
+// });
+/////////////////////////////////////////////////////////////
+//down from here works with postman aftel lady dlaat fix
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.query._id;
+    const validateValue = await propertiesValidation.validatePropertySchema(
+      req.body
+    );
+
+    // console.log(validateValue);
+    if (validateValue) {
+      const newUserData = await propertiesModel.findByIdAndUpdate(
+        id,
+
+        validateValue.price,
+        validateValue.description,
+        validateValue.address
+        // validateValue.img,
+      );
+      res.json("property upDated  successfully");
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+/////////////////////////////////////////////////////////////
+//until from here works with postman aftel lady dlaat fix
+
+router.delete("/:id", async (req, res) => {
+  const _id = req.query;
+  console.log("req1", req.query);
+  try {
+    console.log("_id", _id);
+
+    // http://localhost:3001/api/properties/:id?_id=636b7fee0673e9925182ac0b
+    //this structure in postman deleted
+
+    const property = await propertiesModel.deleteProperty(_id._id);
+
+    res.json(property);
+
+    if (property) {
+    }
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
+});
+// router.delete("/:id", async (req, res) => {
+//   const _id = req.query;
+//   console.log("req1", req.query);
+//   try {
+//     console.log("_id", _id);
+
+//     const property = await propertiesModel.deleteProperty(_id._id);
+
+//     res.json(property);
+
+//     if (property) {
+//     }
+//   } catch (err) {
+//     res.json(err);
+//     console.log(err);
+//   }
+// });
+
+module.exports = router;
---
+++ property server/properties.model.js
@@ -0,0 +1,109 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+/* create user schema */ const propertiesSchema = new Schema({
+  // img: { type: Image, required: false },
+  // img: { type: String },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+//create conllection
+//all the mulipulation on the documents will be using this object
+const Properties = mongoose.model("Properties", propertiesSchema);
+//this function will create new user
+
+const insertProperty = (price, description, address) => {
+  const property = new Properties({
+    // img:
+    //   req.body.img ??
+    //   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    // img,
+
+    price,
+    description,
+    address,
+  });
+
+  return property.save();
+};
+
+//////////////////////////////////////////////////
+// from here up loading property with img from multer on html work--- nor react
+// const Schema = mongoose.Schema;
+// /* create user schema */ const propertiesSchema = new Schema({
+//   // img: { type: Image, required: false },
+//   img: { type: String },
+//   price: { type: Number, required: true },
+//   description: { type: String, required: true },
+//   address: { type: String, required: true },
+// });
+
+// //create conllection
+// //all the mulipulation on the documents will be using this object
+// const Properties = mongoose.model("Properties", propertiesSchema);
+// //this function will create new user
+// const insertProperty = (img, price, description, address) => {
+//   const property = new Properties({
+//     // img:
+//     //   req.body.img ??
+//     //   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+//     img,
+
+//     price,
+//     description,
+//     address,
+//   });
+
+//   return property.save();
+// };
+
+const selectAllProperties = () => {
+  return Properties.find();
+};
+
+const selectPropertyById = (_id) => {
+  return Properties.find({ _id });
+};
+const selectPropertyByAddress = (filter) => {
+  return Properties.find({ address: { $eq: filter.address } });
+};
+const selectPropertyByMaxPrice = (filter) => {
+  return Properties.find({ price: { $lte: filter.price } });
+};
+const selectPropertyByMinPrice = (filter) => {
+  return Properties.find({ price: { $gte: filter.price } });
+};
+
+const deleteProperty = (_id) => {
+  return Properties.findOneAndDelete({ _id });
+};
+// const findByIdAndUpdate = (_id) => {
+//   const data = { price: price, description: description, address: address };
+//   return Properties.findByIdAndUpdate(_id, {
+//     data,
+//   });
+// };
+//////////////////////////////////////
+// dwon from here works on postman
+const findByIdAndUpdate = (_id, price, description, address) => {
+  return Properties.findByIdAndUpdate(_id, {
+    price: price,
+    description: description,
+    address: address,
+  });
+};
+//////////////////////////////////////////
+// until from here works on postman
+
+module.exports = {
+  Properties,
+  insertProperty,
+  selectAllProperties,
+  selectPropertyById,
+  deleteProperty,
+  selectPropertyByAddress,
+  selectPropertyByMaxPrice,
+  selectPropertyByMinPrice,
+  findByIdAndUpdate,
+};
